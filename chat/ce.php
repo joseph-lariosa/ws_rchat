213754c1322b43745bb9b5d64cf0a9fdbdd9500f
Joseph Lariosa <git@josephlariosa.com>
2020-04-14 11:28:30 +0800

    Fixed CE

---
+++ chat/ce.php
@@ -0,0 +1,36 @@
+<?php include('../config.php');?>
+<?php $query = mysqli_query($conn, "SELECT * FROM chat LEFT JOIN `users` ON users.userid=chat.userid WHERE chat_room_id='1' ORDER BY chatid DESC LIMIT 20 ") or die(mysqli_error());
+			while ($row = mysqli_fetch_array($query)) {
+			?>
+
+
+				<div class="chat-entry card p-1 m-1">
+					<div class="row">
+						<div class="col-md-12">
+							<div class="text-muted float-right d-flex">
+								<div class="time-stamp-h">
+									<i class="fa fa-clock-o" title="<?php echo date("l,h:m A", strtotime($row['chat_date'])); ?>"></i>
+								</div>
+							</div>
+							<div class="d-flex">
+
+								<div class="c-left mr-2">
+									<div class="dp-wrapper">
+										<a href="#" data-toggle="modal" data-target="#<?php echo $row["username"]; ?>">
+											<img class="rounded" src="<?php if ($row['img_url'] != '') { echo "uploads/" . $row['img_url'] . ""; } else {echo "images/default-person.png";} ?>">
+										</a>
+									</div>
+									<span class="mt-1 d-block badge badge-<?php echo $row['role']; ?>"><?php echo $row['role']; ?></span>
+								</div>
+
+
+								<div class="c-right">
+									<a href="#" class="text-white font-weight-bold" id="us" onclick="changeValue(this)"><?php echo $row['username']; ?></a><br><?php echo $row['chat_msg']; ?>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+
+			<?php } ?>
--- index.php
+++ index.php
@@ -58,45 +58,7 @@ include('template/header.php');
 		</nav>
 
 
-		<div id="chat_output" class="scroll_lock default_scroll text-white">
-
-			<?php $query = mysqli_query($conn, "SELECT * FROM chat LEFT JOIN `users` ON users.userid=chat.userid WHERE chat_room_id='1' ORDER BY chat_date DESC LIMIT 40 ") or die(mysqli_error());
-			while ($row = mysqli_fetch_array($query)) {
-			?>
-
-
-				<div class="chat-entry card p-1 m-1">
-					<div class="row">
-						<div class="col-md-12">
-							<div class="text-muted float-right d-flex">
-								<div class="time-stamp-h">
-									<i class="fa fa-clock-o" title="<?php echo date("l,h:m A", strtotime($row['chat_date'])); ?>"></i>
-								</div>
-							</div>
-							<div class="d-flex">
-
-								<div class="c-left mr-2">
-									<div class="dp-wrapper">
-										<a href="#" data-toggle="modal" data-target="#<?php echo $row["username"]; ?>">
-											<img class="rounded" src="<?php if ($row['img_url'] != '') { echo "uploads/" . $row['img_url'] . ""; } else {echo "images/default-person.png";} ?>">
-										</a>
-									</div>
-									<span class="mt-1 d-block badge badge-<?php echo $row['role']; ?>"><?php echo $row['role']; ?></span>
-								</div>
-
-
-								<div class="c-right">
-									<a href="#" class="text-white font-weight-bold" id="us" onclick="changeValue(this)"><?php echo $row['username']; ?></a><br><?php echo $row['chat_msg']; ?>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-
-
-			<?php } ?>
-
-		</div>
+		<div id="chat_output" class="scroll_lock default_scroll text-white d-flex flex-column-reverse"></div>
 
 
 		<form id="chatbox" class="forms pl-1 pr-1 mt-1 form-lg">
@@ -106,6 +68,8 @@ include('template/header.php');
 				</div>
 				<input type="text" id="chat_input" class="form-control input-lg" placeholder="Say something..." autocomplete="off">
 				<input type="hidden" id="1">
+				<input type="hidden" id="user_name" value="<?php echo $_SESSION['userName'];?>">
+				<input type="hidden" id="user_id" value="<?php echo $_SESSION['userID'];?>">
 				<div class="input-group-append">
 					<button class="btn btn-primary" id="send_msg" type="submit">
 						<i class="fa fa-paper-plane" aria-hidden="true"></i>
@@ -137,78 +101,5 @@ include('template/header.php');
 
 </div>
 <!-- page-wrapper -->
-
-<script>
-	jQuery(function($) {
-
-		// Websocket
-		var websocket_server = new WebSocket("ws://rchat.test:8080/");
-
-		websocket_server.onopen = function(e) {
-			$('#widget-right').load('chat/widget-right.php');
-			console.log("Connection established!");
-			websocket_server.send(
-				JSON.stringify({
-					'type': 'socket',
-					'user_id': <?php echo $userID; ?>,
-					'user_name': "<?php echo $userName; ?>"
-				})
-			);
-		};
-
-		websocket_server.onclose = function(e) {
-			$('#widget-right').load('chat/widget-right.php');
-			console.log("User Disconnected");
-		};
-
-		websocket_server.onerror = function(e) {
-			// Errorhandling
-		}
-		websocket_server.onmessage = function(e) {
-			$('#widget-right').load('chat/widget-right.php');
-			var json = JSON.parse(e.data);
-			console.log(e.data);
-			switch (json.type) {
-				case 'chat':
-					$('#chat_output').append(json.msg);
-					autoScrolling();
-					break;
-			}
-		}
-
-
-		$('#send_msg').on('click', function() {
-			if ($('#chat_input').val() == "") {
-				alert('Please write message first');
-				event.preventDefault();
-
-			} else {
-				event.preventDefault();
-				var chat_msg = $('#chat_input').val();
-				var room_id = $('#room_id').val();
-				websocket_server.send(
-					JSON.stringify({
-						'type': 'chat',
-						'user_id': <?php echo $userID; ?>,
-						'user_name': "<?php echo $userName; ?>",
-						'chat_msg': chat_msg
-					})
-				);
-				$.ajax({
-					type: "POST",
-					url: "chat/send_message.php",
-					data: {
-						msg: chat_msg,
-						id: room_id,
-					},
-					success: function() {
-						autoScrolling_msg();
-					}
-				});
-				$("input[type='text']").val('');
-			}
-		});
-	});
-</script>
 
 <?php include('template/footer.php'); ?>
